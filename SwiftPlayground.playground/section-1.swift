70a60e96c5ad2f94abdf289ad844bb3df1acec25
Justin Lynch <justinmatthewlynch@gmail.com>
2014-11-24 00:21:36 -0500

    init

---
+++ SwiftPlayground.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let string = "Hello"
+
+println("Hello, world")
+println(str)
+println(string)
+
+let expFlt: Float = 4
+
+let strg = 54
+
+println(String(strg))
+
+
